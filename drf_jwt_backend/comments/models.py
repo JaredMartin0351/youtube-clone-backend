c6f5b4948893ca9f974e8e2fd70041186b579a08
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-01-18 14:50:51 -0600

    Create Reply class / models

--- drf_jwt_backend/comments/models.py
+++ drf_jwt_backend/comments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Comment(models.Model):
     user = models.CharField(User, on_delete=models.CASCADE)
--- drf_jwt_backend/drf_jwt_backend/local_settings.py
+++ drf_jwt_backend/drf_jwt_backend/local_settings.py
@@ -9,7 +9,7 @@ DATABASES = {
         'ENGINE': 'mysql.connector.django',
         'NAME': 'drf_jwt_database',
         'USER': 'root',
-        'PASSWORD': 'Aghalebi33',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
