6abf3b582de2b8d4bbf8c92568411b640546592d
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-01-18 14:59:09 -0600

    Add Comment serializer

--- drf_jwt_backend/comments/views.py
+++ drf_jwt_backend/comments/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_cars(request):
+    comments = Comments.objects.all()
+    serializer = CommentsSerializer(comments,many=True)
+    return Response(serializer.data)
