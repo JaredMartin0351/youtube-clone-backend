fa64730edc0d08131bc283f1e132210eb39aee9e
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-01-19 09:47:10 -0600

    added foreignkey prop to user under comments

--- drf_jwt_backend/comments/models.py
+++ drf_jwt_backend/comments/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Comment(models.Model):
-    user = models.CharField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     video_id = models.CharField(max_length=50)
     text = models.CharField(max_length=500)
     likes = models.IntegerField()
