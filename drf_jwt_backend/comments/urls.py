4b8cedda218b736b389e59b9fcc7513566764eab
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-01-19 10:44:16 -0600

    changed comments urls to accept our comments function

--- drf_jwt_backend/comments/urls.py
+++ drf_jwt_backend/comments/urls.py
@@ -5,7 +5,7 @@ from comments import views
 
 
 urlpatterns = [
-    path('all/', views.CommentList.as_view()),
+    path('all/', views.get_all_comments),
     path('', views.create_comment),
     path('', views.get_all_replies),
 ]
