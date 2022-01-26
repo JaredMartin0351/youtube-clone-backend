2602d84c1985a5f117db5be419848d30fd3e8546
JaredMartin0351 <jaredmartin0351@gmail.com>
2022-01-26 14:54:00 -0600

    change to serializers to get user info

--- drf_jwt_backend/comments/serializers.py
+++ drf_jwt_backend/comments/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from .models import Comment, Reply 
+from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, read_only=True)
     class Meta:
         model = Comment
-        fields = ['id','user', 'video_id', 'text', 'likes', 'dislikes']
+        fields = '__all__'
         
 class ReplySerializer(serializers.ModelSerializer):
+    user=UserSerializer(many=False, read_only=True)
     class Meta:
         model = Reply 
-        fields = ['id','user', 'comment', 'text', ] 
+        fields = '__all__'
